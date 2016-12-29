f55755579c5bf20e204032572503528a7cde22bc
Martin R. Hristov <martin.r.hristov@sap.com>
2016-12-29 12:05:50 +0200

    Ruby app template improved

--- web/application.rb
+++ web/application.rb
@@ -1,23 +1,11 @@
-require "rubygems"
-require "bundler/setup"
-require "sinatra"
-require File.join(File.dirname(__FILE__), "environment")
+require 'sinatra'
 
-configure do
-  set :views, "#{File.dirname(__FILE__)}/views"
-  set :show_exceptions, :after_handler
+# require_relative 'routes/users'
+
+enable :sessions
+
+get '/' do
+  erb :home
 end
 
-configure :production, :development do
-  enable :logging
-end
-
-helpers do
-  # add your helpers here
-end
-
-# root page
-get "/" do
-  @profiles = Profile.all
-  erb :root
-end
+# DataMapper.auto_upgrade!
